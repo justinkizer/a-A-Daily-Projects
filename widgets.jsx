600976575d747a0793906f85fd09758660361f95
Justin Kizer <justinkizer@users.noreply.github.com>
2017-03-03 16:07:24 -0800

    Reorginization

--- widgets.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Tabs from './tabs';
-import Clock from './clock';
-
-document.addEventListener("DOMContentLoaded", () => {
-	const root = document.getElementById("root");
-	ReactDOM.render(<Clock/>, root);
-});
